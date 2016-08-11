acfb66cda8d83bbfae6580e2ea38988294b351d8
jiangman517 <642241105@qq.com>
2016-08-11 09:56:34 +0800

    数据中心-师生信息-住宿信息

--- app/scripts/controllers/flat/selectByFlat.js
+++ app/scripts/controllers/flat/selectByFlat.js
@@ -13,7 +13,7 @@ angular.module('flatpcApp')
         $scope.$apply();
     }
     var a = document.createElement('a');
-    a.href = AppConfig.FRAME + "index.php?m=Coredata&c=CollegeClass&a=stay&token=="+AppConfig.token+"&schoolcode="+AppConfig.schoolCode;
+    a.href = AppConfig.FRAME + "index.php?m=Coredata&c=CollegeClass&a=stay&token="+AppConfig.token+"&schoolcode="+AppConfig.schoolCode;
     a.target="page-frame";
     a.click();
 }]);
